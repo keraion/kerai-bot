df26f49d86ed8967f558f466120142b3083d70f8
Cameron <105471409+keraion@users.noreply.github.com>
2024-08-16 16:47:45 -0400

    initial bot setup

---
+++ src/keraibot/bot.py
@@ -0,0 +1,31 @@
+import sys
+import logging
+
+from keraibot.errors import InvalidTokenError, NoTokenError
+from keraibot.auth import validate, refresh_token
+
+bot_logger = logging.getLogger("kerai-bot")
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+
+def main():
+    try:
+        try:
+            validate()
+        except InvalidTokenError:
+            # Can we refresh the token?
+            refresh_token()
+    except NoTokenError as e:
+        bot_logger.error(f"Not authorized, try running `kerai-bot auth`")
+        sys.exit(1)
+
+    bot_logger.info("Authorized, starting bot...")
+    # TODO: the rest of the owl.
+
+
+if __name__ == "__main__":
+    main()
