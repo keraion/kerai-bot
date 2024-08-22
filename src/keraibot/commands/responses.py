a7b9922ccc1cc4b0b77340ed169e29f9be5cd04d
Cameron <105471409+keraion@users.noreply.github.com>
2024-08-21 23:47:08 -0400

    clean up shoutout

--- src/keraibot/commands/responses.py
+++ src/keraibot/commands/responses.py
@@ -5,15 +5,21 @@ from keraibot.core.commands import ChatCommand, Permission
 
 bot_logger = logging.getLogger("kerai-bot.response")
 
+
 def shoutout(*args):
     user = args[0]
     bot_logger.info(f"{user}, {args}")
-    channel_info = TWITCH_API.channel_info_by_login(user).json()
-    bot_logger.info(f"{channel_info}")
+    channel_info_response = TWITCH_API.channel_info_by_login(user)
+    if not channel_info_response.ok:
+        bot_logger.error(f"Couldn't find user {user}.")
+        return
+    channel_info = channel_info_response.json()
+    user_login = channel_info["data"][0]["broadcaster_login"]
+    user_name = channel_info["data"][0]["broadcaster_name"]
     category = channel_info["data"][0]["game_name"]
     if category:
         return (
-            f"You should probably check out {user} at https://twitch.tv/{user} "
+            f"You should probably check out {user_name} at https://twitch.tv/{user_login} "
             f"they were last playing {category}!"
         )
 
